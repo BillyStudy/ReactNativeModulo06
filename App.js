473522f1070d0981dc74b3bd950f8337c8324bad
BillyStudy <billystudy@hotmail.com>
2020-03-09 18:27:59 -0300

    First commit

---
+++ App.js
@@ -0,0 +1,31 @@
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" backgroundColor={"#fafafa"} />
+      <SafeAreaView>
+          
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
