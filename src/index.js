a2613f31149722803e641900d28a650243f23220
BillyStudy <billystudy@hotmail.com>
2020-03-10 15:09:10 -0300

    Config Reactotron

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { SafeAreaView, StatusBar } from 'react-native';
+
+import './config/ReactotronConfig';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" backgroundColor={"#fafafa"} />
+      <SafeAreaView>
+          
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
